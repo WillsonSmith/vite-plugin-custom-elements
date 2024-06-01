ea80ffa1972cd1f597be93d0f8a0c0b1f2529e51
Willson Smith <me@willsonsmith.com>
2024-06-01 16:34:03 -0400

    adding a quick demo to consume later

--- plugin/plugin-custom-element.ts
+++ plugin/plugin-custom-element.ts
@@ -1,8 +1,12 @@
+import { getTagName } from '@web/parse5-utils';
+import { readFile } from 'node:fs/promises';
 import path from 'node:path';
-import { parse } from 'parse5';
+import { parse, parseFragment } from 'parse5';
 
 import { generateManifest } from './manifest';
 import { findCustomElements } from './parsers';
+import { findHtmlElementFiles } from './parsers/HtmlCustomElements/findHtmlElementFiles/findHtmlElementFiles';
+import { parseHtmlElement } from './parsers/HtmlCustomElements/parseHtmlElement/parseHtmlElement';
 
 const cwd = process.cwd();
 
@@ -22,9 +26,25 @@ export function pluginCustomElement({
       const document = parse(content);
       const elements = findCustomElements(document);
 
-      const manifest = await generateManifest(projectDir);
-      console.log(elements);
-      console.log(manifest);
+      const htmlCustomElementFiles = await findHtmlElementFiles(
+        path.join(projectDir, elementDir),
+      );
+
+      const included = htmlCustomElementFiles.filter((elementFile) => {
+        return elements.find((el) => elementFile.includes(getTagName(el)));
+      });
+
+      const parsed = await Promise.all(
+        included.map(async (el) => {
+          return {
+            tagName: path.basename(el).split('.html')[0],
+            parts: parseHtmlElement(parseFragment(await readFile(el, 'utf8'))),
+          };
+        }),
+      );
+
+      console.log(parsed);
+
       return content;
     },
   };

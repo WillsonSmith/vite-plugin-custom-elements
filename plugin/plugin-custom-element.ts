a072fcd3dbdc032d0df2e6431a31cc9251e36561
Willson Smith <me@willsonsmith.com>
2024-06-03 19:01:48 -0400

    Transforming script imports

--- plugin/plugin-custom-element.ts
+++ plugin/plugin-custom-element.ts
@@ -4,16 +4,14 @@ import {
   createScript,
   findElement,
   getAttribute,
-  getAttributes,
+  getChildNodes,
   getTagName,
 } from '@web/parse5-utils';
-import { Package } from 'custom-elements-manifest';
 import path from 'node:path';
 import { parse, serialize } from 'parse5';
-import { Document } from 'parse5/dist/tree-adapters/default';
+import { Document, DocumentFragment } from 'parse5/dist/tree-adapters/default';
 
 import { generateManifest, getCustomElementsFromManifest } from './manifest';
-import { DefinedElement } from './manifest/getCustomElementsFromManifest/getCustomElementsFromManifest';
 import { findCustomElements } from './parsers';
 import { findHtmlElementFiles } from './parsers/HtmlCustomElements/findHtmlElementFiles/findHtmlElementFiles';
 import { injectStyles } from './parsers/HtmlCustomElements/injectStyles/injectStyles';
@@ -54,6 +52,8 @@ export function pluginCustomElement({
       replaceElementsContent(parsedElements, document);
       injectStyles(parsedElements, document);
 
+      injectScripts(projectDir, parsedElements, document);
+
       const hydrateScripts = await generateHydrationScripts(
         projectDir,
         customElements,
@@ -66,6 +66,61 @@ export function pluginCustomElement({
       return serialize(document);
     },
   };
+}
+
+function injectScripts(
+  rootDir: string,
+  elements: RequiredElement[],
+  root: Element | Document | DocumentFragment,
+) {
+  const scripts: Element[] = [];
+  for (const element of elements) {
+    const scriptTags = element.parsed.scriptTags;
+    if (scriptTags.length === 0) continue;
+
+    const scriptSources = new Set<string>();
+    const scriptContents = new Set<string>();
+
+    for (const tag of scriptTags) {
+      const src = getAttribute(tag, 'src');
+
+      if (src) {
+        console.log(src);
+      }
+
+      const first = getChildNodes(tag)[0];
+      if (first.nodeName === '#text') {
+        const relativePath = path.dirname(element.path).split(rootDir)[1];
+        scriptContents.add(transformScriptImports(relativePath, first.value));
+      }
+    }
+
+    for (const script of Array.from(scriptContents)) {
+      const scriptElement = createScript({ type: 'module' }, script);
+      appendChild(root, scriptElement);
+    }
+  }
+}
+
+function transformScriptImports(relativePath: string, scriptString: string) {
+  const staticImportRegex =
+    /import\s+((?:[\w*\s{},]*\s*from\s*)?['"])([^'"]+)(['"])/;
+  const dynamicImportRegex = /(import\s*\(\s*['"])([^'"]+)(['"]\s*\))/g;
+
+  let value = scriptString;
+  value = value.replace(
+    staticImportRegex,
+    (_: string, p1: string, importPath: string, p3: string) => {
+      return `import ${p1}${path.join(relativePath, importPath)}${p3}`;
+    },
+  );
+  value = value.replace(
+    dynamicImportRegex,
+    (_: string, p1: string, importPath: string, p3: string) => {
+      return `${p1}${path.join(relativePath, importPath)}${p3}`;
+    },
+  );
+  return value;
 }
 
 async function generateHydrationScripts(

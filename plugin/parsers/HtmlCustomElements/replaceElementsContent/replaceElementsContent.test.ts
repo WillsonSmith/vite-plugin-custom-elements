0cfdd938bdcf449914730a3d0e672ae1463e1122
Willson Smith <me@willsonsmith.com>
2024-06-03 13:34:02 -0400

    add some tests for html replacer

---
+++ plugin/parsers/HtmlCustomElements/replaceElementsContent/replaceElementsContent.test.ts
@@ -0,0 +1,72 @@
+import { RequiredElement } from '../parseRequiredHtmlElements/parseRequiredHtmlElements';
+import {
+  appendChild,
+  createDocumentFragment,
+  createElement,
+  findElement,
+  getChildNodes,
+} from '@web/parse5-utils';
+import { parseFragment, serialize } from 'parse5';
+import { describe, expect, it } from 'vitest';
+
+import { replaceElementsContent } from './replaceElementsContent';
+import { findTag } from '@/plugin/util/parse5';
+
+describe('replaceElementsContent', () => {
+  it('Replaces custom element content', async () => {
+    const xTagContent = `<div class="x-tag">My tag</div>`;
+    const replacers = [
+      {
+        path: '',
+        tagName: 'x-tag',
+        parsed: {
+          styleTags: [],
+          scriptTags: [],
+          content: parseFragment(xTagContent),
+        },
+      },
+    ] as unknown as RequiredElement[];
+
+    const frag = createDocumentFragment();
+    const el = createElement('x-tag');
+    appendChild(frag, el);
+
+    replaceElementsContent(replacers, frag);
+    expect(getChildNodes(el).length).toBe(1);
+  });
+
+  it('Replaces nesed custom element content', async () => {
+    const xTagContent = `<div class="x-tag"><x-tag-2></x-tag-2></div>`;
+    const xTag2Content = `<div class="x-tag-2">My second tag</div>`;
+    const replacers = [
+      {
+        path: '',
+        tagName: 'x-tag',
+        parsed: {
+          styleTags: [],
+          scriptTags: [],
+          content: parseFragment(xTagContent),
+        },
+      },
+      {
+        path: '',
+        tagName: 'x-tag-2',
+        parsed: {
+          styleTags: [],
+          scriptTags: [],
+          content: parseFragment(xTag2Content),
+        },
+      },
+    ] as unknown as RequiredElement[];
+
+    const frag = createDocumentFragment();
+    const el = createElement('x-tag');
+
+    appendChild(frag, el);
+
+    replaceElementsContent(replacers, frag);
+    expect(getChildNodes(el).length).toBe(1);
+
+    expect(findElement(frag, findTag('x-tag-2'))).not.toBeFalsy();
+  });
+});
---
+++ plugin/parsers/HtmlCustomElements/replaceElementsContent/replaceElementsContent.ts
@@ -0,0 +1,55 @@
+import { findCustomElements } from '../../findCustomElements/findCustomElements';
+import { RequiredElement } from '../parseRequiredHtmlElements/parseRequiredHtmlElements';
+import {
+  appendChild,
+  findElement,
+  getChildNodes,
+  getParentNode,
+  getTagName,
+  insertBefore,
+  remove,
+} from '@web/parse5-utils';
+import { parseFragment, serialize } from 'parse5';
+import { Document, DocumentFragment } from 'parse5/dist/tree-adapters/default';
+
+import { findTag } from '@/plugin/util/parse5';
+
+export function replaceElementsContent(
+  replacers: RequiredElement[],
+  root: Document | DocumentFragment | Element,
+) {
+  const customElements = findCustomElements(root);
+
+  for (const customElement of customElements) {
+    const tag = getTagName(customElement);
+    const replacer = replacers.find((replacer) => {
+      return replacer.tagName === tag;
+    });
+
+    if (replacer) {
+      const cloned = cloneNode(replacer.parsed.content);
+      replaceElementsContent(replacers, cloned);
+
+      // TODO: Handle multiple slots
+      const slot = findElement(cloned, findTag('slot'));
+      const elementChildren = getChildNodes(customElement);
+      if (slot) {
+        for (const child of elementChildren) {
+          insertBefore(getParentNode(slot), child, slot);
+        }
+        remove(slot);
+      }
+
+      for (const child of elementChildren) {
+        remove(child);
+      }
+      for (const child of getChildNodes(cloned)) {
+        appendChild(customElement, child);
+      }
+    }
+  }
+}
+
+function cloneNode(fragment: DocumentFragment) {
+  return parseFragment(serialize(fragment));
+}
